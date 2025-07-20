f7d3917b24044a80dddc79780451dafd02015e99
Oybek Xoshimboyev <oybek@example.com>
2025-07-20 14:44:13 +0500

    allowed hostsss

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = [
+    'railway.app',
+    '127.0.0.1',
+    'localhost',
+]
+
 
 
 
